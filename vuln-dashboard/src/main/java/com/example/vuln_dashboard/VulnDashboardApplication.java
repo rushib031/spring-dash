3a7ffb7088dc4f8b9ba94199d20acf301f4d464e
Rushi Bhatt <81031434+rushib031@users.noreply.github.com>
2026-01-12 14:35:24 -0500

    initial project structure

---
+++ vuln-dashboard/src/main/java/com/example/vuln_dashboard/VulnDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuln_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulnDashboardApplication.class, args);
+	}
+
+}
---
+++ vuln-dashboard/src/test/java/com/example/vuln_dashboard/VulnDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuln_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
